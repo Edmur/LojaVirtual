ae3601a4b77c5a89b9da77e1b23bbf270bd7c171
Edmur Serafim Junior <edmur.serafim@hotmail.com>
2020-05-06 00:01:22 -0300

    Criação das Entidades

---
+++ LojaVirtual.Dominio/Entidades/ItemPedido.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaVirtual.Dominio.Entidades
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    
+    }
+}
---
+++ LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+    }
+}
